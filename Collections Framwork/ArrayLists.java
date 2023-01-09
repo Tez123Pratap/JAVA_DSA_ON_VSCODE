87e2fe940ca35f31e4af7e33ac94a527322c32ce
Tez <65640383+Tez123Pratap@users.noreply.github.com>
2023-01-09 10:53:01 +0530

    Create ArrayList

---
+++ Collections Framwork/ArrayLists.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayLists{
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(0);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
